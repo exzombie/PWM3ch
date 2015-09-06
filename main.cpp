6b226c811d3d5d957a92fdfe3f9ba822c1604d63
Jure Varlec <jure@varlec.si>
2015-09-06 12:30:27 +0200

    Add a licence header to main.cpp.

--- main.cpp
+++ main.cpp
@@ -1,3 +1,21 @@
+/*
+    PWM3ch, a three-channel PWM LED controller with color presets.
+    Copyright (C) 2015 Jure Varlec <jure.varlec@ad-vega.si>
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include "FastPin.h"
 #include <util/delay.h>
 #include <avr/interrupt.h>
