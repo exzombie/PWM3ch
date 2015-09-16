a2cdeeedac79e1a08aedf01db9427c78cd981083
Jure Varlec <jure@varlec.si>
2015-09-16 20:37:46 +0200

    firmware: HSV operation.

--- main.cpp
+++ main.cpp
@@ -26,47 +26,80 @@ using Color = byte[3];
 static const byte debounceMs = 50; // Loop delay.
 static const byte nColors = 5;  // The number of color presets.
 
-static const FastAnyPin unconnectedPins[] = {4, 5};
+static const FastAnyPin unconnectedPins[] = {4, 5, 6}; // ICSP, actually.
 
 // PWM pins.
 static const FastPin<8> red;
 static const FastPin<9> green;
-static const FastPin<6> blue;
+static const FastPin<3> blue;
 
 // Analog pins.
-static const FastPin<0> lumPot;
-static const FastPin<1> redPot;
-static const FastPin<2> greenPot;
-static const FastPin<3> bluePot;
-static const FastAnyPin analogPins[] = {
-    lumPot, redPot, greenPot, bluePot
-};
+static const FastPin<0> huePot;
+static const FastPin<1> satPot;
+static const FastPin<2> valPot;
 
 static const FastPin<7> colorBtn;
 static const FastPin<10> settingSw;
 
 // The current color as a choice in the EEPROM-stored table.
 static byte currentColor = 0;
-// The current color, dimmed by the potentiometer.
-static Color currentColorValue = {0, 0, 0};
-// The current color, undimmed.
-static Color currentColorRaw = {0, 0, 0};
+// The current color in both representations.
+static Color currentColorHSV = {0, 0, 0};
+static Color currentColorRGB = {0, 0, 0};
 
-static void setBrightness(byte brightness)
+static void HSV2RGB()
 {
-    for (byte i = 0; i < 3; ++i) {
-	currentColorValue[i] =
-	    (currentColorRaw[i] * brightness) / 256;
+    // Algorithm based on the Wikipedia article on HSV colorspace.
+    // Hue is shifted to make potentiometer extremes blue.
+    float h_, s, v, c, x, m, r, g, b;
+    h_ = (float)(currentColorHSV[0] + 170) * (6. / 256.); // Open interval.
+    s = (float)currentColorHSV[1] / 255.;         // Closed interval.
+    v = (float)currentColorHSV[2] / 255.;         // Closed interval.
+    c = v * s;
+    x = c * (1. - fabs(fmodf(h_, 2) - 1.));
+    byte choice = floor(h_);
+    switch (choice) {
+    case 0:
+	r = c; g = x; b = 0;
+	break;
+    case 1:
+	r = x; g = c; b = 0;
+	break;
+    case 2:
+	r = 0; g = c; b = x;
+	break;
+    case 3:
+	r = 0; g = x; b = c;
+	break;
+    case 4:
+	r = x; g = 0; b = c;
+	break;
+    case 5:
+	r = c; g = 0; b = x;
+	break;
+    default:
+	r = 0; g = 0; b = 0;
     }
+    m = v - c;
+    r = 255 * (r + m);
+    g = 255 * (g + m);
+    b = 255 * (b + m);
+
+    // Use temporaries to reduce glitching to minimum.
+    volatile byte rv, gv, bv;
+    rv = r; gv = g; bv = b;
+    currentColorRGB[0] = rv;
+    currentColorRGB[1] = gv;
+    currentColorRGB[2] = bv;
 }
 
-// Color table access.
+// Color table access. Only stores hue and saturation.
 static void readColor()
 {
-    EEAR = 3 * currentColor;
-    for (byte i = 0; i < 3; ++i) {
+    EEAR = 2 * currentColor;
+    for (byte i = 0; i < 2; ++i) {
 	FAST_SET(EECR, EERE);
-	currentColorRaw[i] = EEDR;
+	currentColorHSV[i] = EEDR;
 	++EEAR;
     }
 }
@@ -74,9 +107,9 @@ static void readColor()
 static void writeColor()
 {
     EECR = 0;
-    EEAR = 3 * currentColor;
-    for (byte i = 0; i < 3; ++i) {
-	EEDR = currentColorRaw[i];
+    EEAR = 2 * currentColor;
+    for (byte i = 0; i < 2; ++i) {
+	EEDR = currentColorHSV[i];
 	FAST_SET(EECR, EEMPE);
 	FAST_SET(EECR, EEPE);
 	while (FAST_GET(EECR, EEPE));
@@ -84,17 +117,17 @@ static void writeColor()
     }
 }
 
-// Normal mode: measure lumPot to adjust brighness, check buttons to
+// Normal mode: measure valPot to adjust brighness, check buttons to
 // change colors or enter setting mode.
 static void normalLoop();
 
-// Setting mode: measure all pots to adjust both color and brightness,
-// save to eeprom.
+// Setting mode: measure all pots to fully adjust color, save to
+// eeprom.
 static void setColorLoop();
 
 static inline byte analogRead(byte pin)
 {
-    if (pin > 3)
+    if (pin > 2)
 	return 0;
     ADMUX = pin; // Select the pin.
     FAST_SET(ADCSRA, ADSC); // Start converting.
@@ -109,17 +142,15 @@ int main()
 	pin.input();
 	pin.high();
     }
-    for (auto& pin: analogPins) {
-	pin.input();
-	pin.low();
-    }
+    for_pin([](auto pin) { pin.input(); pin.low(); },
+	    huePot, satPot, valPot);
     for_pin([](auto pin) { pin.output(); pin.low(); },
 	    red, green, blue);
     for_pin([](auto pin) { pin.input(); pin.high(); },
 	    colorBtn, settingSw);
 
     // Disable digital input on analog pins.
-    DIDR0 = _BV(ADC0D) | _BV(ADC1D) | _BV(ADC2D) | _BV(ADC3D);
+    DIDR0 = _BV(ADC0D) | _BV(ADC1D) | _BV(ADC2D);
     ADCSRA = _BV(ADPS2); // Prescaling 16x.
     ADCSRB |= _BV(ADLAR); // Left-adjust the result for 8-bit read.
     ADCSRA |= _BV(ADEN) | _BV(ADSC); // Enable and initialize the ADC.
@@ -129,10 +160,12 @@ int main()
     FAST_SET(TIMSK0, TOIE0); // Enable overflow interrupt.
     FAST_SET(TCCR0B, CS00); // Active, no prescaler.
 
-    sei();
-
     readColor();
+    for (byte i = 0; i < 3; ++i)
+	currentColorRGB[i] = 0;
+    sei();
     normalLoop();
+
     return 0;
 }
 
@@ -144,9 +177,9 @@ static void normalLoop()
 
     while (true) {
 	_delay_ms(debounceMs);
-	// Use the current color preset dimmed to the required brightness.
-	byte brightness = analogRead(lumPot);
-	setBrightness(brightness);
+	// Use the current color preset, but value from pot.
+	currentColorHSV[2] = analogRead(valPot);
+	HSV2RGB();
 
 	// Read the button and switch and debounce.
 	bool buttonPressed = ! colorBtn.get();
@@ -176,10 +209,9 @@ static void setColorLoop()
 	_delay_ms(debounceMs);
 	// Same as the main loop, except the current color is read from pots.
 	byte i = 0;
-	for_pin([&i](auto pin) { currentColorRaw[i++] = analogRead(pin); },
-		redPot, greenPot, bluePot);
-	byte brightness = analogRead(lumPot);
-	setBrightness(brightness);
+	for_pin([&i](auto pin) { currentColorHSV[i++] = analogRead(pin); },
+		huePot, satPot, valPot);
+	HSV2RGB();
 
 	// If the button is pressed, overwrite the current color preset.
 	bool buttonPressed = ! colorBtn.get();
@@ -188,7 +220,8 @@ static void setColorLoop()
 	    // Blink write confirmation;
 	    for (byte i = 0; i < 5; ++i) {
 		_delay_ms(300);
-		setBrightness((i%2) * 255);
+		currentColorHSV[2] = (i%2) * 255;
+		HSV2RGB();
 	    }
 	} else {
 	    buttonPrevious = buttonPressed;
@@ -206,10 +239,10 @@ ISR(TIM0_OVF_vect, ISR_BLOCK)
 	for_pin([](auto pin) { pin.high(); }, red, green, blue);
     }
 
-    if (pwmCounter >= currentColorValue[0])
+    if (pwmCounter >= currentColorRGB[0])
     	red.low();
-    if (pwmCounter >= currentColorValue[1])
+    if (pwmCounter >= currentColorRGB[1])
     	green.low();
-    if (pwmCounter >= currentColorValue[2])
+    if (pwmCounter >= currentColorRGB[2])
     	blue.low();
 }
